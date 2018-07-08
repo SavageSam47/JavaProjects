f7446348ba70a63cd4effca709c32043920b819c
timotree3 <timorcb@gmail.com>
2018-07-08 17:00:46 -0400

    Second

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello World");
+		System.out.println("Hello Again");
+		System.out.print("Ooof");
+	}
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 12;
+		System.out.println(age);
+		String name = ("Sam");
+		System.out.println(name);
+		System.out.println("Hi my name is " + name + "and i am " + age);
+	}
+
+}

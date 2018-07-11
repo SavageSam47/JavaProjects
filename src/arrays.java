ea08552148b4abfe2d3c4f209e6118343276eca6
timotree3 <timorcb@gmail.com>
2018-07-11 13:33:18 -0400

    last 7.11.18

---
+++ src/arrays.java
@@ -0,0 +1,20 @@
+
+public class arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String [] array= new String [6];
+		array[0] = "hello";
+		array[1] = "russellisrude";
+		array[2] = "theemojimovieisgood";
+		array[3] = "nomansskyisgood";
+		array[4] = "russellisbadatdogeball";
+		array[5] = "russellisreallybadatdogeball";
+		for (int x=0; x<6; x++) {
+		System.out.println(array[x]);
+		}
+
+	}
+
+}
---
+++ src/shoppinglist.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class shoppinglist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		Scanner scan2=new Scanner(System.in);
+		System.out.println("How many items would you like to put on the list?");
+		int itemnumbers = scan.nextInt();
+		String [] array= new String [itemnumbers];
+		for (int x=0; x<itemnumbers;x++) {
+			System.out.println("Enter an item: ");
+			String items = scan2.nextLine();
+			array[(x)]=items;
+		}
+		System.out.println("Would you like to see all of your items or one? (Say all/one)");
+		String answer = scan.next();
+		if (answer.equalsIgnoreCase("all")) {
+			for (int x=0; x<itemnumbers +1;x++) {
+				System.out.println(array[x]);
+			}
+		}
+		else if (answer.equalsIgnoreCase("one")) {
+			System.out.println("Enter Item number:");
+			int itemnumberchoosen = scan.nextInt();
+			System.out.println(array[itemnumberchoosen -1]);
+		}
+	}
+
+}

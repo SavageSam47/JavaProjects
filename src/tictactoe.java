db4e301eeb27f352b4eedc33e6d2dc9317a6d4f7
timotree3 <timorcb@gmail.com>
2018-07-12 13:26:36 -0400

    last 7.12.18

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -12,7 +12,7 @@ public class tictactoe {
 				board[x][y]="- ";
 			}
 		}
-		while (loop<18) {
+		while (loop<9) {
 
 			for (int x=0; x<3; x++) {
 				for(int y=0; y<3; y++) {
@@ -21,31 +21,41 @@ public class tictactoe {
 				System.out.println();
 
 			}
-			System.out.println("Player X type coordnate one:");
-			int xp1 = scan.nextInt();
-			System.out.println("Player X type coordnate two:");
-			int yp1 = scan.nextInt();
-			board [xp1][yp1]="X ";
-			loop++;
+			int loop1 = 0;
+			while (loop1==0) {
+				System.out.println("Player X type coordnate one:");
+				int xp1 = scan.nextInt();
+				System.out.println("Player X type coordnate two:");
+				int yp1 = scan.nextInt();
+
+				
+				if (board[xp1][yp1].equals("O ") || board[xp1][yp1].equals("X ")) {
+					System.out.println("That spot is already taken. Try again.");
+				}
+				else {
+					loop1++;
+					loop++;
+					board [xp1][yp1]="X ";
+				}
+
+			}
+			//Check all
 			if (board[0][0].equals("X ") && board[0][1].equals("X ") && board[0][2].equals("X ")||
 					board[1][0].equals("X ") && board[1][1].equals("X ") && board[1][2].equals("X ")||
 					board[2][0].equals("X ") && board[2][1].equals("X ") && board[2][2].equals("X ")) {
 				System.out.println("Player X wins");
-				loop++;
-				return;
+				break;
 			}
 			if (board[0][0].equals("X ") && board[1][0].equals("X ") && board[2][1].equals("X ")||
 					board[0][1].equals("X ") && board[1][1].equals("X ") && board[2][1].equals("X ")||
 					board[0][2].equals("X ") && board[1][2].equals("X ") && board[2][2].equals("X ")) {
 				System.out.println("Player X wins");
-				loop++;
-				return;
+				break;
 			}
 			if (board[0][0].equals("X ") && board[1][1].equals("X ") && board[2][2].equals("X ")||
 					board[0][2].equals("X ") && board[1][1].equals("X ") && board[2][0].equals("X ")) {
 				System.out.println("Player X wins");
-				loop++;
-				return;
+				break;
 			}
 			for (int x=0; x<3; x++) {
 				for(int y=0; y<3; y++) {
@@ -54,28 +64,43 @@ public class tictactoe {
 				System.out.println();
 
 			}
-			System.out.println("Player O type coordnate one:");
-			int xp2 = scan.nextInt();
-			System.out.println("Player O type coordnate two:");
-			int yp2 = scan.nextInt();
-			board [xp2][yp2] ="O ";
-			loop++;
+			if (loop==9) {
+				System.out.println("Its a Tie.");
+				break;
+			}
+			int loop2 = 0;
+			while (loop2 == 0) {
+				System.out.println("Player O type coordnate one:");
+				int xp2 = scan.nextInt();
+				System.out.println("Player O type coordnate two:");
+				int yp2 = scan.nextInt();
+
+
+				if (board[xp2][yp2].equals("X ")||board[xp2][yp2].equals("O ")) {
+					System.out.println("That spot is already taken. Try again.");
+				}
+				else {
+					loop2++;
+					loop++;
+					board [xp2][yp2] ="O ";
+				}
+			}
 			if (board[0][0].equals("O ") && board[0][1].equals("O ") && board[0][2].equals("O ")||
 					board[1][0].equals("O ") && board[1][1].equals("O ") && board[1][2].equals("O ")||
 					board[2][0].equals("O ") && board[2][1].equals("O ") && board[2][2].equals("O ")) {
 				System.out.println("Player O wins");
-				loop++;
+				break;
 			}
 			if (board[0][0].equals("O ") && board[1][0].equals("O ") && board[2][1].equals("O ")||
 					board[0][1].equals("O ") && board[1][1].equals("O ") && board[2][1].equals("O ")||
 					board[0][2].equals("O ") && board[1][2].equals("O ") && board[2][2].equals("O ")) {
 				System.out.println("Player O wins");
-				loop++;
+				break;
 			}
 			if (board[0][0].equals("O ") && board[1][1].equals("O ") && board[2][2].equals("O ")||
 					board[0][2].equals("O ") && board[1][1].equals("O ") && board[2][0].equals("O ")) {
 				System.out.println("Player O wins");
-				loop++;
+				break;
 			}
 
 

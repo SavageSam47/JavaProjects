a4de8c3bf7773dc6ea657527a756b542e1558f32
SummerTech <SummerTech@10.144.4.175>
2018-07-19 13:37:29 -0400

    oof1

--- src/medianchecker.java
+++ src/medianchecker.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class bubblesorting {
+public class medianchecker {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,20 +8,28 @@ public class bubblesorting {
 		System.out.println("How many numbers would you like to input?");
 		int oof = scan.nextInt();
 		int [] array= new int [oof];
-		
+
 		for (int a = 0; a<oof; a++) {
 			System.out.println("Enter Value for position " + (a + 1));
 			array [a] = scan.nextInt();
 		}
 		arraychecker(array, oof);
 		printarray(array, oof);
-		
+		int median1 = oof%2;
+		if(median1==1) {
+			int median2 = oof - (oof/2);
+			System.out.println();
+			System.out.println(array[median2]);
+		}
+		else if(median1==0) {
+
+		}
 
 
 	}
 	public static int[] printarray(int [] array, int oof) {
 		for(int x = 0; x<oof; x++) {
-			System.out.print(array[x]+"   ");
+			System.out.println(array[x]+"   ");
 		}
 		return array;
 	}

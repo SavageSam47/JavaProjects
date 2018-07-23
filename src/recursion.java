4ec0537131367db3967e9b0dc77c9f5c93650aec
Alex2262 <alex2262alexander@gmail.com>
2018-07-23 11:01:25 -0400

    second 7.23.18

--- src/recursion.java
+++ src/recursion.java
@@ -3,19 +3,44 @@ public class recursion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(fibonacci(100));
 
-		int x = 2000;
-		System.out.println(recursion(x));
 	}
 
-	
-	
-	public static int recursion(int number) {
-		if(number==1) {
-			return 1;
+	public static int mult(int num1, int num2) {
+		if(num2==1) {
+			return num1;
 		}
 		else {
-			return recursion (number-1)+number;
+			return num1+ mult(num1,num2-1);
+		}
+
+	}
+	public static long exponent(long base, long power) {
+		if (power==1){
+			return base;
+		}
+		else {
+			return base* exponent(base, power-1);
 		}
 	}
+	public static long factorial(long base) {
+		if(base == 1) {
+			return base;
+		}
+		else {
+			return base* factorial(base-1);
+		}
+	}
+	public static long fibonacci(long n)  {
+	    if(n <=1)
+	        return n;
+	   else
+	      return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+	
+
+
+
+
 }

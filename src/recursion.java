6f093c162286121ba399c4a3703f9b1e13c7fc59
Alex2262 <alex2262alexander@gmail.com>
2018-07-24 09:58:52 -0400

    first 7.24.18

--- src/recursion.java
+++ src/recursion.java
@@ -3,7 +3,7 @@ public class recursion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(fibonacci(100));
+		System.out.println(fibonacci(50));
 
 	}
 
---
+++ src/towersofannoying.java
@@ -0,0 +1,20 @@
+
+public class towersofannoying {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// smallest disk- 0 medium - 1 large - 22
+		oof(4,1,2,3);
+	}
+	public static void oof(int disk,int startpeg,int spare,int dest) {
+		if (disk==1) {
+			System.out.println("Move disk " + disk + " from peg " + startpeg + " to peg " + dest);
+		}
+		else {
+			oof(disk-1, startpeg, dest, spare);
+			System.out.println("Move disk " + disk + " from peg " + startpeg + " to peg " + dest);
+			oof(disk-1, spare, startpeg, dest);
+
+		}
+	}
+}

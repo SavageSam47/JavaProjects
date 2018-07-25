3efe7420852742618ca3555866aa92e0a6d9c22b
Alex2262 <alex2262alexander@gmail.com>
2018-07-25 10:00:44 -0400

    first7.25.18

--- src/car.java
+++ src/car.java
@@ -4,7 +4,9 @@ public class car {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		car car1=new car("V-cilinder","flextape",5);
+		car1.setEngine("flex seal");
 		System.out.print(car1.getBrand());
+
 	}
 	
 	
@@ -22,7 +24,7 @@ public class car {
 		mpg=mg;
 	}
 	
-	public String getengineType() {
+	public String getEngineType() {
 		return engineType;
 	}
 	public String getBrand() {
@@ -31,5 +33,17 @@ public class car {
 	public int getMPG() {
 		return mpg;
 	}
+	
+	public void setEngine(String change_engine) {
+		engineType=change_engine;
+	}
+	public void setBrand(String change_brand) {
+		brand=change_brand;
+	}
+	public void setMPG(int change_mpg) {
+		mpg=change_mpg;
+	}
+	
+	
 	
 }

088a2e59e73cbd9dc3fd99797e0debe87da914df
Alex2262 <alex2262alexander@gmail.com>
2018-07-26 13:26:32 -0400

    last 7.26.18

--- src/deck.java
+++ src/deck.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class deck {
 
 	public static void main(String[] args) {
@@ -6,24 +6,37 @@ public class deck {
 
 		card [] deck=new card[52];
 		int b=0;
-		for(int c=2; c<14;c++,b++) {
+		for(int c=2; c<15;c++,b++) {
 			deck[b]= new card("Hearts",c);
 		}
-		int d =14; 
-		for(int c=2; c<14;c++,d++) {
+		int d =13; 
+		for(int c=2; c<15;c++,d++) {
 			deck[d]= new card("Diamonds",c);
 		}
-		int e = 28;
-		for(int c=2; c<14;c++,e++) {
+		int e = 26;
+		for(int c=2; c<15;c++,e++) {
 			deck[e]= new card("Spades",c);
 		}
-		int f = 42;
-		for(int c=2; c<14;c++,f++) {
+		int f = 39;
+		for(int c=2; c<15;c++,f++) {
 			deck[f]= new card("Clubs",c);
 		}
+		shuffle(deck);
 		for(int c=0; c<52; c++) {
 			System.out.println(deck[c]);
 		}
 	}
 
+	public static card[] shuffle(card [] deck) {
+		Random ran=new Random();
+		for (int c=0;c<5000;c++) {
+			int number =ran.nextInt(52);
+			int number2 = ran.nextInt(52);
+			card a = deck[number];
+			card b = deck[number2];
+			deck[number] = b;
+			deck[number2]= a;
+		}
+		return deck;
+	}
 }

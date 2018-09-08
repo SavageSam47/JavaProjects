2ac9994e297cbb2313f91f701fd99267fe83316d
coditum <coditum@10.144.4.25>
2018-09-08 13:43:17 -0400

    again

--- src/networking/client.java
+++ src/networking/client.java
@@ -1,1 +1,44 @@
-a
+package networking;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class client {
+
+	
+	
+	public static void main(String[] args) {
+		Socket s = null;
+		Scanner scan = new Scanner(System.in);
+		
+	try {
+		
+		s = new Socket("127.0.0.1",25565);
+		BufferedReader n = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		BufferedWriter i = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+		while (s.isConnected()){
+			String input = scan.next();
+			i.write(input);
+			i.newLine();
+			i.flush();
+			System.out.println(n.readLine());
+		}
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+
+	
+	}
+	
+
+}

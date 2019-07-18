d5b097aa9477854977a46010587fbd652a4f44f4
SummerTech <stusr@summertech.net>
2019-07-18 11:16:55 -0400

    Heaps completed

--- src/heap.java
+++ src/heap.java
@@ -24,25 +24,38 @@ public class heap {
         }
     }
 
+    public static ArrayList<Integer> heapSort(ArrayList<Integer> toSort) {
+        ArrayList<Integer> sorted = new ArrayList<Integer>();
+        heap heap = new heap(toSort);
+        int initalSize = toSort.size();
+        for (int x = 0; x < initalSize; x++) {
+            sorted.add(heap.removeHead());
+        }
+        return sorted;
+    }
+
     private void heapifyDownward(int index) {
         boolean t = true;
-        if (index * 2 + 1 > list.size()) {
+        if (index * 2 + 1 >= list.size()) {
             return;
         } else {
             while (t == true) {
-                if (list.get(index) < list.get(index * 2 + 1) && list.get(index) < list.get(index * 2 + 2)) {
+                if (index * 2 + 1 >= list.size() - 1) {
+                    return;
+                } else if (list.get(index) < list.get(index * 2 + 1) && list.get(index) < list.get(index * 2 + 2)) {
                     t = false;
                     return;
                 } else {
                     if (list.get(index * 2 + 1) > list.get(index * 2 + 2)) {
                         Collections.swap(list, index, index * 2 + 2);
                         index = index * 2 + 2;
-                        t = false;
+
                     } else if (list.get(index * 2 + 1) < list.get(index * 2 + 2)) {
                         Collections.swap(list, index, index * 2 + 1);
                         index = index * 2 + 1;
-                        t = false;
+
                     }
+
                 }
             }
         }
@@ -50,6 +63,13 @@ public class heap {
 
     public heap() {
         list = new ArrayList<Integer>();
+    }
+
+    public heap(ArrayList<Integer> listInput) {
+        list = listInput;
+        for (int x = 0; x < list.size() - 1; x++) {
+            heapify(x);
+        }
     }
 
     public void add(int value) {
@@ -63,30 +83,23 @@ public class heap {
 
     public int removeHead() {
         int newHead = list.get(0);
-        Collections.swap(list, 0, list.size()-1);
+        Collections.swap(list, 0, list.size() - 1);
         list.remove(list.size() - 1);
         heapifyDownward(0);
         return newHead;
     }
 
-    public void remove(int value) {
-        int temp = list.indexOf(value);
-        int replacewith = list.get(list.size() - 1);
-        list.set(list.indexOf(value), list.get(list.size() - 1));
+    public int remove(int index) {
+        int newHead = list.get(index);
+        list.add(Integer.MIN_VALUE);
+        Collections.swap(list, index, list.size() - 1);
         list.remove(list.size() - 1);
-        while (true) {
-            if (list.indexOf(replacewith) * 2 > list.size()) {
-                break;
-            } else {
-                if (list.get(list.indexOf(replacewith) * 2 - 1) > list.get(list.indexOf(replacewith) * 2)) {
-                    temp = list.indexOf(replacewith) * 2;
-                } else if (list.get(list.indexOf(replacewith) * 2 + 1) < list.get(list.indexOf(replacewith) * 2)) {
-                    temp = list.indexOf(replacewith) * 2 + 1;
-                }
-
-                Collections.swap(list, temp, list.indexOf(replacewith) - 1);
-            }
-        }
+        heapify(index);
+        removeHead();
+        // // Collections.swap(list, index, list.size() - 1);
+        // // list.remove(list.size() - 1);
+        // // heapifyDownward(0);
+        return newHead;
     }
 
     public void print() {

36a0ef89e6442be3b4c2795975d3a9a2171a6f1e
Sam Sherman <samsherman@pop-os.localdomain>
2019-08-10 14:29:33 -0400

    Added user functionality for passwordgen.java, edited robot.java for use on linux.

--- src/passwordgen.java
+++ src/passwordgen.java
@@ -6,7 +6,9 @@ public class passwordgen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter password length");
 		int length = scan.nextInt();
+		scan.close();
 		System.out.println(passwordgenerator(length));
 	}
 	
--- src/robot.java
+++ src/robot.java
@@ -9,29 +9,26 @@ public class robot {
 
 	public static void main(String[] args) throws IOException, AWTException, InterruptedException {
 		// TODO Auto-generated method stub
-
-		Desktop.getDesktop().open(new File("/Applications/TextEdit.app"));
+		Runtime run = Runtime.getRuntime();
+		run.exec("gedit");
 		Robot r = new Robot();
-		r.mouseMove(400, 500);
-		Thread.sleep(15);
-		r.mouseMove(400, 300);
-		
-		r.mouseMove(400, 500);
-		/**r.keyPress(KeyEvent.VK_H);
+		Thread.sleep(500);
+		r.keyPress(KeyEvent.VK_H);
+		r.keyRelease(KeyEvent.VK_H);
 		Thread.sleep(15);
 		r.keyPress(KeyEvent.VK_E);
+		r.keyRelease(KeyEvent.VK_E);
 		Thread.sleep(15);
 		r.keyPress(KeyEvent.VK_L);
+		r.keyRelease(KeyEvent.VK_L);
 		Thread.sleep(15);
 		r.keyPress(KeyEvent.VK_L);
+		r.keyRelease(KeyEvent.VK_L);
 		Thread.sleep(15);
-		r.keyPress(KeyEvent.VK_O);**/
+		r.keyPress(KeyEvent.VK_O);
+		r.keyRelease(KeyEvent.VK_O);
 		
 		
-		
-		r.keyPress(KeyEvent.VK_L);
-		Thread.sleep(15);
-		r.keyRelease(KeyEvent.VK_L);		Thread.sleep(15);
 
 
 	}

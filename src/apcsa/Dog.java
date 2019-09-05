0a4995c0421956db04f8bce8b9a17ef7b43f87cf
1116525 <1116525@9032L42203XWHS.education.com>
2019-09-05 11:06:22 -0400

    Dog Class

---
+++ src/apcsa/Dog.java
@@ -0,0 +1,33 @@
+package apcsa;
+
+public class Dog {
+
+	private String name;
+	private int age;
+	private double weight;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
+	
+	public void setName(String nn) {
+		name = nn;
+	}
+	
+	public void setAge(int nn) {
+		age = nn;
+	}
+	
+	public void setWeight(double nn) {
+		weight = nn;
+	}
+	
+}
---
+++ src/apcsa/DogTest.java
@@ -0,0 +1,19 @@
+package apcsa;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog d = new Dog();
+		Dog nd = new Dog();
+		
+		d.setName("Happy");
+		d.setAge(3);
+		d.setWeight(20.5);
+		
+		nd.setName("Sad");
+		nd.setAge(4);
+		nd.setWeight(24.7);
+	}
+
+}
